2f79a04f60d61df4b61be4fe1a0210980dded35b
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2017-02-17 10:44:55 +0100

    feat(centos-ncdigloop): provide more options through env vars

--- centos-ncdigloop/ncdig-loop.sh
+++ centos-ncdigloop/ncdig-loop.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-
+#
+# Deal with defaults and overrides
+#
 sleep_time=${SLEEP_TIME:-15}
 dig_options=${DIG_OPTIONS:-"+noall +answer"}
-nc_option=${NC:-"-v -w1 -i1"}
+nc_options=${NC_OPTIONS:-"-v -w1 -i1"}
+ping_options=${PING_OPTIONS:-"-c 1"}
+do_dig=${DO_DIG:-1}
+do_nc=${DO_NC:-1}
+do_ping=${DO_PING:-1}
+#
 echo "Found NCDIG_TARGETS=${NCDIG_TARGETS}"
-
+echo
+echo "sleep_time [$sleep_time]"
+echo "dig_options [$dig_options]"
+echo "nc_options [$nc_options]"
+echo "ping_options [$ping_options]"
+echo "do_dig [$do_dig] do_nc [$do_nc] do_ping [$do_ping]"
 function get_me_out() {
   echo "Ctrl+c trapped, exiting because you asked"
   exit 0
@@ -35,13 +47,22 @@ while true; do
   do
     host=$(echo ${target} | cut -d":" -f1)
     port=$(echo ${target} | cut -d":" -f2)
-    echo "Digging and NCing [$target/host=$host,port=$port]"
-    echo "dig start --------------------->"
-    dig $host $dig_options
-    echo "<--------------------- dig end"
-    echo "nc start --------------------->"
-    nc $nc_option $host $port
-    echo "<---------------------nc end"
+    echo "Checking [$target/host=$host,port=$port]"
+    if [ $do_dig == 1 ]; then
+      echo "dig start --------------------->"
+      dig $host $dig_options
+      echo "<--------------------- dig end"
+    fi
+    if [ $do_nc == 1 ]; then
+      echo "nc start --------------------->"
+      nc $nc_options $host $port
+      echo "<---------------------nc end"
+    fi
+    if [ $do_ping == 1 ]; then
+      echo "ping start --------------------->"
+      ping $ping_options $host
+      echo "<--------------------- ping end"
+    fi
     echo
   done
   echo "Sleeping ${sleep_time} seconds"
